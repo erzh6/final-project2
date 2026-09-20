9d790a42c2fc900c2c83dfd69a2ed521a1f562ac
Erai <erjandoktur6@gmail.com>
2026-09-21 00:05:37 +0600

    updated

--- src/test/java/com/example/finalproject/MapperTest.java
+++ src/test/java/com/example/finalproject/MapperTest.java
@@ -1,10 +1,26 @@
 package com.example.finalproject;
 
+import com.example.finalproject.dto.StudentResponse;
+import com.example.finalproject.mapper.Mapper;
+import com.example.finalproject.model.Student;
 import org.junit.jupiter.api.Test;
 
-public class MapperTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MapperTest {
+
     @Test
+    void toStudentResponse_worksCorrectly() {
+        Student student = new Student();
+        student.setId(1L);
+        student.setFirstName("Азамат");
+        student.setLastName("Кубанычбеков");
+        student.setEmail("azamat.k@example.com");
 
+        StudentResponse response = Mapper.toStudentResponse(student);
 
-
+        assertEquals(1L, response.getId());
+        assertEquals("Азамат", response.getFirstName());
+        assertEquals("azamat.k@example.com", response.getEmail());
+    }
 }
