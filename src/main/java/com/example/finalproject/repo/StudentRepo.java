3679e29e13121fa35027d934334398f46bffd85c
Erai <erjandoktur6@gmail.com>
2026-09-20 23:32:01 +0600

    updated

--- src/main/java/com/example/finalproject/repo/StudentRepo.java
+++ src/main/java/com/example/finalproject/repo/StudentRepo.java
@@ -3,11 +3,13 @@ package com.example.finalproject.repo;
 import com.example.finalproject.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface StudentRepo extends JpaRepository<Student, Long> {
-   List<Student> searchByName(String name);
+   @Query
+   List<Student> searchByName(@Param("name") String name);
 }
