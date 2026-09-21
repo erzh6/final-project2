0791f160f92be5d48cfb8c4bda2ac70285611dc2
Erai <erjandoktur6@gmail.com>
2026-09-21 11:18:17 +0600

    updated

--- src/main/java/com/example/finalproject/repo/EnrollmentRepo.java
+++ src/main/java/com/example/finalproject/repo/EnrollmentRepo.java
@@ -3,6 +3,7 @@ package com.example.finalproject.repo;
 import com.example.finalproject.model.Enrollment;
 import com.example.finalproject.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.Optional;
 @Repository
 public interface EnrollmentRepo extends JpaRepository<Enrollment, Long> {
     Optional<Enrollment> findByStudentIdAndCourseId(Long studentId, Long courseId);
-
+@Query("select u from Enrollment u where u.student.id = ? ")
     List<Enrollment> findByStudentId(Long studentId);
 
     List<Enrollment> findByCourseId(Long courseId);
